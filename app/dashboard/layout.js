32aa6c628f14ddfa9e115c3778f197fcffe6b409
寒焰 <hanyanxx1@163.com>
2025-05-14 14:48:24 +0800

    4.6. 定义错误处理（Error Handling）

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,3 +1,3 @@
 export default function DashboardLayout({ children }) {
-  return <section>{children}</section>;
+  return <div>{children}</div>;
 }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,13 +1,13 @@
-import { use } from "react";
+"use client";
+// dashboard/page.js
+import React from "react";
 
-async function getData() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return {
-    message: "Hello, Dashboard!",
+export default function Page() {
+  const [error, setError] = React.useState(false);
+
+  const handleGetError = () => {
+    setError(true);
   };
-}
-export default async function DashboardPage(props) {
-  // const { message } = await getData();
-  const { message } = use(getData());
-  return <h1>{message}</h1>;
+
+  return <>{error ? Error() : <button onClick={handleGetError}>Get Error</button>}</>;
 }

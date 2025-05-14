abe31e79d50a2587d19e9475e1404f1f20937a53
寒焰 <hanyanxx1@163.com>
2025-05-14 14:21:17 +0800

    4.5. 定义加载界面（Loading UI）

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <h1>Hello, Dashboard!</h1>;
+import { use } from "react";
+
+async function getData() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return {
+    message: "Hello, Dashboard!",
+  };
+}
+export default async function DashboardPage(props) {
+  // const { message } = await getData();
+  const { message } = use(getData());
+  return <h1>{message}</h1>;
 }

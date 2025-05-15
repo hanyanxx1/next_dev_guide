88ef23af9cb94c0036c3a77c17c3f55df4b52597
寒焰 <hanyanxx1@163.com>
2025-05-15 17:58:19 +0800

    实战：控制请求数

--- middleware.js
+++ middleware.js
@@ -1,9 +1,21 @@
 import { NextResponse } from "next/server";
+import { RateLimiter } from "limiter";
+const limiter = new RateLimiter({ tokensPerInterval: 3, interval: "min", fireImmediately: true });
 
-export function middleware(request) {
-  return NextResponse.redirect(new URL("/blog/yayu_middleware", request.url));
+export async function middleware(request) {
+  const remainingRequests = await limiter.removeTokens(1);
+  console.log("Remaining requests:", remainingRequests);
+  if (remainingRequests < 0) {
+    return new NextResponse(JSON.stringify({ success: false, message: "Too Many Requests" }), {
+      status: 429,
+      headers: { "content-type": "application/json" },
+    });
+  }
+
+  return NextResponse.next();
 }
 
+// 设置匹配路径
 export const config = {
-  matcher: "/blog/yayu",
+  matcher: "/api/chat",
 };

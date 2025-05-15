5d5f1d007355a3a760b98ee0bf939859a5e39bed
寒焰 <hanyanxx1@163.com>
2025-05-15 16:06:18 +0800

    1. 定义路由处理程序

---
+++ app/api/posts/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
+
+export async function POST(request, context) {
+  const article = await request.json();
+
+  return NextResponse.json(
+    {
+      id: Math.random().toString(36).slice(-8),
+      data: article,
+    },
+    { status: 201 }
+  );
+}

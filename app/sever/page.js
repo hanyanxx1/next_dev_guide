70b2847602462cf4417eda43543b45687de83ccf
寒焰 <hanyanxx1@163.com>
2025-05-19 14:58:39 +0800

    1. 静态渲染（Static Rendering）

---
+++ app/sever/page.js
@@ -0,0 +1,7 @@
+export const revalidate = 10;
+
+export default async function Page() {
+  const url = (await (await fetch("https://api.thecatapi.com/v1/images/search")).json())[0].url;
+
+  return <img src={url} width="300" alt="cat" />;
+}
